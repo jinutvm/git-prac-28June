b40f58525bacc8e2b7899e0c7778ec03d4c1ede4
jinu babu <jinutvm@gmail.com>
2023-06-28 12:06:37 +0530

    Ver2.2

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log('This is Ver1.3.2')
-console.log('This is Ver2.1')
-console.log('This is Feature2 V1.0')
+console.log('This is Ver2.2')
+console.log('This is Feature2 V1.1')
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.log('This is Ver 2.1')
-console.log('This is Feature2 V1.0')
+console.log('This is Ver 2.2')
+console.log('This is Feature2 V1.1')
